d1b112655ede635aeb2961832cfa9ef19ee6e886
Mord Maman <jaime.mord.maman@gmail.com>
2016-12-14 12:34:10 +0000

    assigns a bgcolor to each div and title to h1

--- script.js
+++ script.js
@@ -4,4 +4,18 @@
 //assign each of the rgb codes to a div
 //make clicking on the winning div win and make clicking on the losing div make the div disappear
 //make background of page change to the colour of the winning code upon selection
+//new colours to allow 6 new colours to be chosen
+//easy vs hard options for 3 or 6 colours
+// start with easy i guess
 
+//array of hex codes
+var rgb = ['rgb(255,0,0)','rgb(0,255,0)','rgb(0,0,255)'];
+
+var divs = document.querySelectorAll('.colourBlock');
+var h1 = document.querySelector('.rgb');
+
+h1.innerHTML = rgb[0];
+
+for(var i = 0; i < divs.length; i++){
+    divs[i].style.backgroundColor = rgb[i];
+}
