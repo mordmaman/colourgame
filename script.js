c71d725ff53f37c5057ebe18985f9ae6d5c1007d
Mord Maman <jaime.mord.maman@gmail.com>
2016-12-14 09:31:11 +0000

    writes notes in scripts.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//array of rgb codes
+//pick 6 rgb codes and store in a new array
+//pick 1 rgb code to be the winning code
+//assign each of the rgb codes to a div
+//make clicking on the winning div win and make clicking on the losing div make the div disappear
+//make background of page change to the colour of the winning code upon selection
+
